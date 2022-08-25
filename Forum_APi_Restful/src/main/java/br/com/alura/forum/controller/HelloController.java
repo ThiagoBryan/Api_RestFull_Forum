8ae4f5b2efc1b1b3669b788e4e7419f5cc443ccc
Thiago <tthurler10@gmail.com>
2022-08-25 16:21:43 -0300

    Api_Restfull_Forum

---
+++ Forum_APi_Restful/src/main/java/br/com/alura/forum/controller/HelloController.java
@@ -0,0 +1,16 @@
+package br.com.alura.forum.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+	
+	@RequestMapping("/")
+	@ResponseBody // apenas para pegar o return
+	public String hello() {
+		return "Hello World";
+	}
+
+}
